030454819a14465bc2324f68cc9c99123ccb5345
eminentpal <eziechukwudi@gmail.com>
2021-03-30 15:42:35 +0100

    added a loader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import Home from "./components/Home";
+//import Home from "./components/Home";
 import {BrowserRouter as Router, Route, Switch  } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 
+import RingLoader from "react-spinners/RingLoader";
+// import About from "./components/About";
+
+const Home = lazy(()=> import("./components/Home"));
+
+// const About = lazy(()=> import("./components/About"));
 
 function App() {
+
+
+
+ 
   return (
     <Router>
       <div className="App">
     <Switch>
       <Route exact path="/" >
-      <Home   />
+      <Suspense fallback={ <div  className="loading"> <RingLoader  loading color="blue" size={80}  /></div>}>
+      <Home  />
+      </Suspense>
+     
       </Route>
-   
+     {/* <Route exact path="/about" >
+     <Suspense fallback={ <div  className="aboutapp"> <RingLoader css={loadingStyle} loading color="blue" size={60}  /></div>}>
+      <About  />
+      </Suspense>
+     </Route> */}
     </Switch> 
     </div>
     </Router>
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="aboutapp" >
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default About
